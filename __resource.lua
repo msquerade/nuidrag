b93cad377a32f2afb1a091fd84ee22844bf8ab95
MsQuerade <msquerade@zoho.eu>
2017-11-24 02:46:41 +0100

    Demo html5 drag doesn't work

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+description "NUI Drag not supported"
+version "1"
+
+client_scripts {
+	"client.lua"
+}
+
+files({
+    "index.html"
+})
+
+ui_page( "index.html" )
---
+++ client.lua
@@ -0,0 +1,3 @@
+AddEventHandler('playerSpawned', function(spawn)
+    SetNuiFocus( true, true );
+end)
